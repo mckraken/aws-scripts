b2d11919292b7a318ebceef3a2ebe8b5547a589d
McKraken <mckraken@mckraken.net>
2016-06-08 01:36:22 -0500

    Change from sys argv to argparse

--- awsiplist.py
+++ awsiplist.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python
 
 from __future__ import print_function
-import netaddr
+
+import argparse
 import json
+
+import netaddr
 import requests
-import sys
 
-region = sys.argv[1]
 
-aws_ip_list_d = json.loads(requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json').text)
+def process_args():
+    parser = argparse.ArgumentParser(
+        description='Get list of Amazon IPs for a region.')
+    parser.add_argument(
+        'region', metavar="AWS_REGION",
+        help='The region for which to get the IP list (e.g. us-west-1).')
 
-ip_region_l = [net['ip_prefix'] for net in aws_ip_list_d['prefixes'] if net['region'] == region]
+    return parser.parse_args()
 
-aws_merged_ip_l = [str(net) for net in netaddr.cidr_merge(ip_region_l)]
+if __name__ == "__main__":
+    region = process_args().region
 
-for item in aws_merged_ip_l:
-    print(item)
+    aws_ip_list_d = json.loads(
+        requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json').text)
 
+    ip_region_l = [net['ip_prefix'] for net in aws_ip_list_d['prefixes']
+                   if net['region'] == region]
+
+    aws_merged_ip_l = [str(net) for net in netaddr.cidr_merge(ip_region_l)]
+
+    for item in aws_merged_ip_l:
+        print(item)
